0fa405298af61abc004a70344f8b1cc4d905c6e5
pbellard <pbellard@PBELLARD-it.it.oracle.com>
2019-05-20 12:16:41 +0200

    Updated Helidon version to 1.1.0 and smaller code fixes

--- src/main/java/io/helidon/demo/CodiceFiscaleResource.java
+++ src/main/java/io/helidon/demo/CodiceFiscaleResource.java
@@ -13,14 +13,12 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/evalcf")
 @RequestScoped
